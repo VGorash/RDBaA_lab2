0d4a5905ce0aa6faac36789c0a6199f131fca10e
V-Gorash <VGorash@luxoft.com>
2021-12-05 23:07:38 +0300

    final lab2

--- src/main/java/Client.java
+++ src/main/java/Client.java
@@ -2,74 +2,101 @@ import java.io.*;
 import java.net.Socket;
 
 public class Client {
+
+    private static final int port = 555;
+    private static final String ip = "127.0.0.1";
+
+    private final BufferedReader serverInput;
+    private final BufferedReader userInput;
+    private final PrintWriter serverOutput;
+    private final Socket socket;
+
     public static void main(String[] args) {
-        int port = 555;
-        String ip = "127.0.0.1";
-
-        BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
         try {
-            Socket socket = new Socket(ip, port);
-            InputStream in = socket.getInputStream();
-            BufferedReader br = new BufferedReader(new InputStreamReader(in));
-            OutputStream out = socket.getOutputStream();
-            PrintWriter pr = new PrintWriter(out, true);
+            new Client().run();
+        }
+        catch (Exception e) {
+            System.out.println("Some kind of error has occurred.");
+            System.exit(0);
+        }
+    }
 
-            System.out.println("""
+    public Client() throws IOException{
+        socket = new Socket(ip, port);
+        serverInput = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        serverOutput = new PrintWriter(socket.getOutputStream(), true);
+        userInput = new BufferedReader(new InputStreamReader(System.in));
+    }
+
+    private void run() throws IOException{
+        System.out.println("Enter 'register' if you want to register, any string otherwise");
+        serverOutput.println(userInput.readLine());
+        boolean logged = false;
+        while (!logged){
+            login();
+            logged = Boolean.parseBoolean(serverInput.readLine());
+        }
+        System.out.println("""
                     Usage:\s
                     when a first symbol on line is ‘>’ – enter operand (number)
                     when a first symbol on line is ‘@’ – enter operation
                     operation is one of ‘+’, ‘-‘, ‘/’, ‘*’ or
                     ‘#’ followed with number of evaluation step
                     ‘q’ to exit\s""");
-            String inp, res;
-            String regex = "\\d+";
+        String inp, res;
+        String regex = "-?\\d+";
+        if(Integer.parseInt(serverInput.readLine()) == 0){
             System.out.print("> ");
-            inp = input.readLine();
+            inp = userInput.readLine();
             while (!inp.matches(regex)) {
                 System.out.println("Please enter only an integer number without spaces or other characters");
                 System.out.print("> ");
-                inp = input.readLine();
+                inp = userInput.readLine();
             }
-            pr.println(inp);
-            System.out.println(br.readLine());
+            serverOutput.println(inp);
+        }
+        System.out.println(serverInput.readLine());
 
-            while (true) {
-                processInput(input, pr, br, socket);
-                res = br.readLine();
-                System.out.println(res);
-            }
-        } catch (Exception e) {
-            System.out.println("Some kind of error has occurred.");
-            System.exit(0);
+        while (true) {
+            processInput();
+            res = serverInput.readLine();
+            System.out.println(res);
         }
     }
 
-    public static void processInput(BufferedReader input, PrintWriter pr, BufferedReader br, Socket socket) throws IOException {
+    private void login() throws IOException{
+        System.out.println("Enter username");
+        serverOutput.println(userInput.readLine());
+        System.out.println("Enter password");
+        serverOutput.println(userInput.readLine());
+    }
+
+    private void processInput() throws IOException {
         System.out.print("@: ");
-        String option = input.readLine();
+        String option = userInput.readLine();
         while (!(option.matches("[-+*/q]") || option.matches("#[\\d+]"))) {
             System.out.println("Please enter an operation which is one of ‘+’, ‘-‘, ‘/’, ‘*’ or\n" +
                     "‘#’ followed with number of evaluation step" +
                     "‘q’ to exit");
             System.out.print("@: ");
-            option = input.readLine();
+            option = userInput.readLine();
         }
-        pr.println(option);
+        serverOutput.println(option);
         if (option.charAt(0) == '#') {
-            System.out.println(br.readLine());
-            processInput(input, pr, br, socket);
+            System.out.println(serverInput.readLine());
+            processInput();
         } else if (option.charAt(0) == 'q') {
             socket.close();
             System.exit(0);
         } else {
             System.out.print("> ");
-            String str = input.readLine();
-            while (!str.matches("\\d+")) {
+            String str = userInput.readLine();
+            while (!str.matches("-?\\d+")) {
                 System.out.println("Please enter an integer number without spaces or other characters");
                 System.out.print("> ");
-                str = input.readLine();
+                str = userInput.readLine();
             }
-            pr.println(str);
+            serverOutput.println(str);
         }
     }
 }
--- src/main/java/Worker.java
+++ src/main/java/Worker.java
@@ -1,40 +1,140 @@
 import java.io.*;
 import java.net.*;
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.sql.*;
 import java.util.*;
 
 public class Worker extends Thread {
-    Socket socket;
-    List<Integer> list = new ArrayList<>();
 
-    public Worker(Socket s) {
+    private static final String jdbcUrl = "jdbc:postgresql://localhost:5432/studs";
+    private static final String jdbcUser = "s264470";
+    private static final String jdbcPassword = "npo508";
+
+    private final Socket socket;
+    private List<Integer> list;
+    private final MessageDigest messageDigest;
+    private final Connection connection;
+    private final BufferedReader input;
+    private final PrintWriter output;
+
+    private String username;
+
+    public Worker(Socket s) throws Exception{
         socket = s;
+        messageDigest = MessageDigest.getInstance("SHA-512");
+        Class.forName("org.postgresql.Driver");
+        connection = DriverManager.getConnection(jdbcUrl, jdbcUser, jdbcPassword);
+        input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        output = new PrintWriter(socket.getOutputStream(), true);
+        list = new ArrayList<>();
+    }
+
+    private void initList() throws SQLException {
+        String query = "SELECT record FROM rdba_calc WHERE user_id = ? ORDER BY seq_num";
+        PreparedStatement preparedStatement = connection.prepareStatement(query);
+        preparedStatement.setString(1, username);
+        ResultSet resultSet = preparedStatement.executeQuery();
+        while (resultSet.next()){
+            list.add(resultSet.getInt(1));
+        }
+    }
+
+    private void register() throws Exception {
+        String username = input.readLine();
+        String password = input.readLine();
+        String query = "INSERT INTO rdba_user(username, password) VALUES (?, ?)";
+        PreparedStatement preparedStatement = connection.prepareStatement(query);
+        preparedStatement.setString(1, username);
+        preparedStatement.setBytes(2, messageDigest.digest(password.getBytes(StandardCharsets.UTF_8)));
+        try {
+            preparedStatement.execute();
+            this.username = username;
+            output.println("true");
+            preparedStatement.close();
+        }
+        catch (SQLException e){
+            output.println("false");
+            preparedStatement.close();
+        }
+    }
+
+    private void login() throws Exception{
+        String username = input.readLine();
+        String password = input.readLine();
+        String query = "SELECT password FROM rdba_user WHERE username = ?";
+        PreparedStatement preparedStatement = connection.prepareStatement(query);
+        preparedStatement.setString(1, username);
+        ResultSet resultSet = preparedStatement.executeQuery();
+        if(!resultSet.next()){
+            output.println("false");
+            resultSet.close();
+            preparedStatement.close();
+            return;
+        }
+        if(!Arrays.equals(messageDigest.digest(password.getBytes(StandardCharsets.UTF_8)), resultSet.getBytes(1))){
+            output.println("false");
+            resultSet.close();
+            preparedStatement.close();
+            return;
+        }
+        output.println("true");
+        resultSet.close();
+        preparedStatement.close();
+        this.username = username;
+    }
+
+    private void addRecord(int value) throws SQLException{
+        String query = "INSERT INTO rdba_calc(user_id, record) VALUES (?, ?)";
+        PreparedStatement preparedStatement = connection.prepareStatement(query);
+        preparedStatement.setString(1, username);
+        preparedStatement.setInt(2, value);
+        preparedStatement.execute();
     }
 
     public void run() {
         try {
-            InputStream in = socket.getInputStream();
-            BufferedReader br = new BufferedReader(new InputStreamReader(in));
-            OutputStream out = socket.getOutputStream();
-            PrintWriter pr = new PrintWriter(out, true);
+            if(input.readLine().equals("register")){
+                while (username == null){
+                    register();
+                }
+            }
+            else{
+                while (username == null){
+                    login();
+                }
+            }
+            initList();
+            output.println(list.size());
 
             int i = 0;
-            int firstInt = Integer.parseInt(br.readLine());
-            list.add(firstInt);
-            pr.println("[#" + (i + 1) + "] = " + list.get(i));
-            i++;
+            int firstInt;
+            if(list.size() == 0){
+                firstInt = Integer.parseInt(input.readLine());
+                list.add(firstInt);
+                addRecord(firstInt);
+                output.println("[#" + (i + 1) + "] = " + list.get(i));
+                i++;
+            }
+            else{
+                i = list.size();
+                firstInt = list.get(i-1);
+                output.println("[#" + i + "] = " + firstInt);
+            }
 
             while (true) {
-                String option = br.readLine();
+                String option = input.readLine();
                 char chOp = option.charAt(0);
                 int result;
 
                 if (chOp == 'q') {
                     break;
                 } else if (chOp == '#') {
-                    firstInt = list.get(Character.getNumericValue(option.charAt(1)) - 1);
+                    firstInt = list.get(Integer.parseInt(option.substring(1)) - 1);
                     list.add(firstInt);
+                    addRecord(firstInt);
                 } else {
-                    int secondInt = Integer.parseInt(br.readLine());
+                    int secondInt = Integer.parseInt(input.readLine());
                     result = switch (chOp) {
                         case '+' -> firstInt + secondInt;
                         case '-' -> firstInt - secondInt;
@@ -43,14 +143,24 @@ public class Worker extends Thread {
                         default -> throw new IllegalStateException("Incorrect");
                     };
                     list.add(result);
+                    addRecord(result);
                     firstInt = result;
                 }
-                pr.println("[#" + (i + 1) + "] = " + list.get(i));
+                output.println("[#" + (i + 1) + "] = " + list.get(i));
                 i++;
             }
             socket.close();
+            connection.close();
+
         } catch (Exception e) {
             System.out.println("Error has occurred in Worker.");
+            try {
+                socket.close();
+                connection.close();
+            }
+            catch (Exception ex){
+                ex.printStackTrace();
+            }
         }
     }
 }
